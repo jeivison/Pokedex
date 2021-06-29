93c262983304b140be81ee9b9d32e1b42d76bcfb
jeivison santos <jeivison1@hotmail.com>
2021-06-28 22:24:49 -0300

    Alterando Lista!

--- app/src/main/java/com/example/pokedexje/MainActivity.java
+++ app/src/main/java/com/example/pokedexje/MainActivity.java
@@ -1,24 +1,29 @@
 package com.example.pokedexje;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Object recyclerView;
-    private Object pokemonAdapter;
+    //private Object recyclerView;
+    //private Object pokemonAdapter;
+
+    private List<Pokemon> pokemonList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = (RecyclerView) findViewById(R.id.rv_pokemons);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_pokemons);
 
-        pokemonAdapter = new PokemonAdapter(pokemonList);
+        PokemonAdapter pokemonAdapter = new PokemonAdapter(pokemonList);
 
         RecyclerView.LayoutManager layoutManager;
         layoutManager = new LinearLayoutManager(getApplicationContext());
@@ -26,7 +31,27 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(pokemonAdapter);
-    }
-}
 
-///// Código 5 — PokemonAdapter.java//////
+        addData();
+
+        }
+
+    private void addData() {
+        Pokemon poke;
+
+        poke = new Pokemon("Bulbasaur", "Grama");
+        pokemonList.add(poke);
+
+        poke = new Pokemon("Charmander", "Fogo");
+        pokemonList.add(poke);
+
+        poke = new Pokemon("Squirtle", "Água");
+        pokemonList.add(poke);
+
+        PokemonAdapter.notifyDataSetChanged();
+    }
+
+    }
+
+
+///// Código 7 — Método addData//////
